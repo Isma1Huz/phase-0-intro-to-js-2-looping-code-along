726d2adc14558d57d21674d7e91876eaf18eab7d
Isma1Huz <ismael.hussein@student.moringaschool.com>
2023-06-18 18:13:45 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+// Code your solutions in this file;
+
+function writeCards(array,action) {
+    const newArray = [];
+    
+  for (let i = 0; i < array.length; i++) {
+    const statement = `Thank you, ${array[i]}, for the wonderful ${action} gift!`
+    newArray.push(statement);
+  }
+  return newArray;
+}
+
+function countDown(num){
+    num = 0
+    while(num <= 10){
+        console.log(num);
+        num++
+    }
+}
